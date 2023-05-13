7bb8145648bd47486f357939ee7c63fa18d712e6
Joaquin Salazar <joaqsala@gmail.com>
2023-05-12 21:28:56 -0500

    comment model completed

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment_on_blog: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },
+    made_comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
